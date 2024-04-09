5e4452218188fbbc85f42130884289561bbf401c
TopDev <kwangxu0924@gmail.com>
2024-04-09 11:40:31 +0200

    fixed UI issue

--- resources/views/livewire/admin/order-list.blade.php
+++ resources/views/livewire/admin/order-list.blade.php
@@ -10,90 +10,91 @@
             Exportar CSV
         </a>
     </div>
-    <table class="w-full mt-5 text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    {{__('IDVENDA')}}
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    {{__('CARTELAS')}}
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    {{__('NOMECILENTE')}}
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    {{__('FONECLIENTE')}}
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    {{__('VALOR')}}
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    {{__('Situação')}}
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    {{__('DATA')}}
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @if(empty($orders) || count($orders) < 1)
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th scope="row" colspan="6" class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Sem dados
+    <div class="overflow-auto w-full">
+        <table class="w-full mt-5 text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        {{__('IDVENDA')}}
                     </th>
-                </tr>            
-            @else
-                @foreach ($orders as $item)
-                    <tr class="cursor-pointer bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
-                            {{$item->id}}
+                    <th scope="col" class="px-6 py-3">
+                        {{__('CARTELAS')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        {{__('NOMECILENTE')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        {{__('FONECLIENTE')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        {{__('VALOR')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        {{__('Situação')}}
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        {{__('DATA')}}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @if(empty($orders) || count($orders) < 1)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="row" colspan="6" class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Sem dados
                         </th>
-                        <td class="px-6 py-4">
-                            {{$item->cardNumbers()}}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{$item->user->name}}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{$item->user->phone}}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{$item->price}}
-                        </td>
-                        <td class="px-6 py-4">
-                            @switch($item->payment_status)
-                                @case(0)
-                                    <p class="border border-yellow_text rounded-lg p-1">
-                                        Aguardando Pagamento
-                                    </p>
-                                    @break
-                                @case(1)
-                                    <p class="border border-sky-500 rounded-lg p-1">
-                                        Pago
-                                    </p>
-                                    @break
-                                @case(2)
-                                    <p class="border border-red-500 rounded-lg p-1">
-                                        Falha no pagamento
-                                    </p>
-                                    @break
-                                @default
-                                    <p class="border border-yellow_text rounded-lg p-1">
-                                        Aguardando Pagamento
-                                    </p>
-                                @endswitch
-                            </p>
-                        </td>
-                        <td class="px-6 py-4">
-                            {{$item->created_at}}
-                        </td>
-                    </tr>
-                @endforeach
-            @endif
-        </tbody>
-    </table>
-
+                    </tr>            
+                @else
+                    @foreach ($orders as $item)
+                        <tr class="cursor-pointer bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+                                {{$item->id}}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{$item->cardNumbers()}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$item->user->name}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$item->user->phone}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$item->price}}
+                            </td>
+                            <td class="px-6 py-4">
+                                @switch($item->payment_status)
+                                    @case(0)
+                                        <p class="border border-yellow_text rounded-lg p-1">
+                                            Aguardando Pagamento
+                                        </p>
+                                        @break
+                                    @case(1)
+                                        <p class="border border-sky-500 rounded-lg p-1">
+                                            Pago
+                                        </p>
+                                        @break
+                                    @case(2)
+                                        <p class="border border-red-500 rounded-lg p-1">
+                                            Falha no pagamento
+                                        </p>
+                                        @break
+                                    @default
+                                        <p class="border border-yellow_text rounded-lg p-1">
+                                            Aguardando Pagamento
+                                        </p>
+                                    @endswitch
+                                </p>
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$item->created_at}}
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+    </div>
     
     <div class="mt-3">
         {{ $orders->links() }}
