d6346899c9396f9fd168604970a0134a607daa01
top_dev <kwangxu0924@gmail.com>
2024-06-20 15:48:45 +0200

    .

--- database/seeders/CSVBingoCardSeeder.php
+++ database/seeders/CSVBingoCardSeeder.php
@@ -13,13 +13,14 @@ class CSVBingoCardSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 1; $i < 21; $i++) {
+        $i =1;
+        // for ($i = 1; $i < 21; $i++) {
             $path = storage_path() . "/csv/$i.CSV";
             $delimiter = ";";
             if (($handle = fopen($path, "r")) === false)
             {
                 print("can't open file $i");
-                break;
+                return;
             }
 
             $csv_headers = fgetcsv($handle, 4000, $delimiter);
@@ -28,24 +29,28 @@ class CSVBingoCardSeeder extends Seeder
 
             while ($row = fgetcsv($handle, 4000, $delimiter))
             {
-                    $bingoCard = array_combine($csv_headers, $row);
-                    if ($nCnt == 1000) {
-                        DB::table('bingo_cards')->insert($bingoCardData);
-                        $nCnt = 0;
-                        $bingoCardData = [];
-                    }
-                    $nCnt++;
-                    $data = [];
-                    for($i = 1; $i < 26; $i++) {
-                        $data["d$i"] = $bingoCard["D$i"];
-                    }
-                    $data['card_number'] = str_pad($bingoCard['CartelaD'], 6, '0', STR_PAD_LEFT);
-                    $data['card_digit'] = $bingoCard['DigitoD'];
-                    $bingoCardData[] = $data;
+                if (count($csv_headers) != count($row)) {
+                    var_dump($row);
+                    continue;
+                }
+                $bingoCard = array_combine($csv_headers, $row);
+                if ($nCnt == 1000) {
+                    DB::table('bingo_cards')->insert($bingoCardData);
+                    $nCnt = 0;
+                    $bingoCardData = [];
+                }
+                $nCnt++;
+                $data = [];
+                for($i = 1; $i < 26; $i++) {
+                    $data["d$i"] = $bingoCard["D$i"];
+                }
+                $data['card_number'] = str_pad($bingoCard['CartelaD'], 6, '0', STR_PAD_LEFT);
+                $data['card_digit'] = $bingoCard['DigitoD'];
+                $bingoCardData[] = $data;
             }
             DB::table('bingo_cards')->insert($bingoCardData);
 
             fclose($handle);
-        }
+        // }
     }
 }
