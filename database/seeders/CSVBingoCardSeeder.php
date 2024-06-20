221ce61692b237cfbf7a24d82d861affc9e19569
top_dev <kwangxu0924@gmail.com>
2024-06-20 16:02:23 +0200

    ...

--- database/seeders/CSVBingoCardSeeder.php
+++ database/seeders/CSVBingoCardSeeder.php
@@ -13,7 +13,7 @@ class CSVBingoCardSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 11; $i < 14; $i++) {
+        for ($i = 14; $i < 17; $i++) {
             $path = storage_path() . "/csv/$i.CSV";
             $delimiter = ";";
             if (($handle = fopen($path, "r")) === false)
